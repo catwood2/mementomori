261d347433ce56b0816f1541a1d8c4066e06d557
catwood2 <catwood2@gmail.com>
2025-06-14 15:47:00 -0500

    Fix Edge Function configuration and routing

--- netlify/edge-functions/airtable-proxy.js
+++ netlify/edge-functions/airtable-proxy.js
@@ -1,12 +1,14 @@
 export default async (request, context) => {
   const url = new URL(request.url);
-  const path = url.pathname.replace('/.netlify/edge-functions/airtable-proxy/', '');
+  const path = url.pathname.replace('/api/', '');
   
   const AIRTABLE_BASE_ID = context.env.AIRTABLE_BASE_ID;
   const AIRTABLE_API_KEY = context.env.AIRTABLE_API_KEY;
   const AIRTABLE_TABLE_NAME = context.env.AIRTABLE_TABLE_NAME;
 
-  const airtableUrl = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}${path}`;
+  const airtableUrl = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${AIRTABLE_TABLE_NAME}${path}${url.search}`;
+  
+  console.log('Proxying to:', airtableUrl);
   
   const response = await fetch(airtableUrl, {
     method: request.method,
@@ -17,5 +19,10 @@ export default async (request, context) => {
     body: request.method !== 'GET' ? await request.text() : undefined
   });
 
-  return response;
+  const data = await response.json();
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
 }; 
